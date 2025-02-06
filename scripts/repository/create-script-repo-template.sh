11914b224150fdea1f86000c5b06d881b32428b3
Xuan Ngo <xuanngo2001@users.noreply.github.com>
2025-02-06 09:30:04 -0500

    Use space instead of tab

--- scripts/repository/create-script-repo-template.sh
+++ scripts/repository/create-script-repo-template.sh
@@ -7,11 +7,11 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 echo "${GV_LOG}>>>>>>>>> Running ${SCRIPT_NAME} ..."
 
 # Explicitly list dependent packages
-#	apt-get install -s -y <your-package> 
+#   apt-get install -s -y <your-package> 
 
 # Install PKG_NAME.
-	apt-get -y install PKG_NAME
+    apt-get -y install PKG_NAME
 
 
 # Log
-	echo "${GV_LOG} * Install PKG_NAME."
+    echo "${GV_LOG} * Install PKG_NAME."
